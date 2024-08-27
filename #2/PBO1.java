73d6eb39f2014f15ef2d82b0d397303516408b82
stanislausarvaa <stanislaus.arva.urian.dante.arianto@students.untidar.ac.id>
2024-08-27 15:18:06 +0700

    Tugas ke 2

---
+++ #2/PBO1.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pbo1.pbo1;
+
+/**
+ *
+ * @author Stanislaus Arva
+ */
+public class PBO1 {
+    
+    
+    public static void main(String[] args) {
+        
+        String Name = "Stanislaus Arva";
+        System.out.println("Nama : " + Name);
+        
+        String NPM = "2320506051";
+        System.out.println("NPM : " + NPM);
+        
+        String Alamat = "Jln. Diakhir pekan lihat kekiri dan kekanan";
+        System.out.println("Alamat : " + Alamat);
+        
+        String no = "08800783282";
+        System.out.println("No Hp : " + no);
+    }
+    
+    
+}
+
+
